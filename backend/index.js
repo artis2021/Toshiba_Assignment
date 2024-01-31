00c23f83086251963e1162d73f679fba805cda63
artis2021 <upasnagupta3333@gmail.com>
2024-01-31 19:28:28 +0530

    Complete project

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'path';
+import apiRoutes from './routes/api.routes.js';
+import cors from 'cors';
+
+const __dirname = path.resolve();
+const app = express();
+
+// Setting up the port
+const PORT = process.env.PORT || 8080;
+
+// Listening to the port
+app.listen(PORT, () => {
+    console.log(`Server running at PORT ${PORT}`);
+});
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'https://toshiba-frontend.vercel.app',
+    optionsSuccessStatus: 200
+}));
+
+// Routes
+app.use('/api', apiRoutes);
+
+// 404 Error
+app.use((req, res) => {
+    res.status(404).json({ message: "404 Not Found, endpoints starts with /api" });
+});
